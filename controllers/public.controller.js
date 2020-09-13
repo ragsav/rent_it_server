165794aabb827909163b4c627c64fa2beed88b8d
ragsav <45696355+ragsav@users.noreply.github.com>
2020-09-13 15:19:14 +0530

    commit23

--- controllers/public.controller.js
+++ controllers/public.controller.js
@@ -18,7 +18,7 @@ exports.getLotController = async (req, res) => {
   const area_id = req.params.area_id;
   if (area_id === "SEND_ALL") {
     Lot.find({ city_id: city_id })
-      .sort({ name: 1 })
+      .sort({ _id: "asc" })
       .exec((err, lots) => {
         if (!err) {
           return res.status(200).send({
@@ -36,7 +36,7 @@ exports.getLotController = async (req, res) => {
       { city_id: city_id, area_id: area_id },
       " -active_plots_list -passive_plots_list"
     )
-      .sort({ name: "asc" })
+      .sort({ _id: "asc" })
       .exec((err, lots) => {
         if (!err) {
           return res.status(200).send({
@@ -57,8 +57,28 @@ exports.getLotWithoutImagesController = async (req, res) => {
   if (area_id === "SEND_ALL") {
     Lot.find(
       { city_id: city_id },
-      "-cover_image -active_plots_list -passive_plots_list",
-      (err, lots) => {
+      " -cover_image -active_plots_list -passive_plots_list"
+    )
+      .sort({ _id: "asc" })
+      .exec((err, lots) => {
+        if (!err) {
+          return res.status(200).send({
+            lots,
+          });
+        } else {
+          console.log(err);
+          return res.status(400).send({
+            error: err,
+          });
+        }
+      });
+  } else {
+    Lot.find(
+      { city_id: city_id, area_id: area_id },
+      " -cover_image -active_plots_list -passive_plots_list"
+    )
+      .sort({ _id: "asc" })
+      .exec((err, lots) => {
         if (!err) {
           return res.status(200).send({
             lots,
@@ -68,25 +88,10 @@ exports.getLotWithoutImagesController = async (req, res) => {
             error: err,
           });
         }
-      }
-    );
-  } else {
-    Lot.find({ city_id: city_id, area_id: area_id }, (err, lots) => {
-      if (!err) {
-        lots.active_plots_list = undefined;
-        lots.passive_plots_list = undefined;
-        lots.cover_image = undefined;
-        return res.status(200).send({
-          lots,
-        });
-      } else {
-        return res.status(400).send({
-          error: err,
-        });
-      }
-    });
+      });
   }
 };
+
 exports.getLotById = async (req, res) => {
   const city_id = req.params.city_id;
   const area_id = req.params.area_id;
